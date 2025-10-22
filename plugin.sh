480967875d0dc90f3465eae0cf7f76f4bf8df5d1
Riley Snyder <rileysnyder@protonmail.ch>
2025-10-22 13:06:14 -0700

    Update plugin.sh

--- plugin.sh
+++ plugin.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
-curl \
-  -X ${PLUGIN_METHOD} \
-  -d ${PLUGIN_BODY} \
-  ${PLUGIN_URL}
+curl -i -X POST \
+  "https://$PLUGIN_HARNESS_URL/v1/catalog/custom-properties/entity?dry_run=false" \
+  -H 'Content-Type: application/json' \
+  -H "Harness-Account: $PLUGIN_HARNESS_ACCOUNT_ID" \
+  -H "x-api-key: $PLUGIN_HARNESS_PLATFORM_API_KEY" \
+  -d '{
+    "property": "'${PLUGIN_PROPERTY}'",
+    "value": "'${PLUGIN_VALUE}'",
+    "mode": "'${PLUGIN_MODE}'",
+    "entity_ref": "'${PLUGIN_ENTITY_REF}'"
+  }'
